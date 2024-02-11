7329847bd5f7dadb6291950ac7dd8be005e62da1
Yakov <yakov@tapuzi.tapuzi>
2024-02-11 14:36:54 +0200

    change auth

--- app/auth/layout.tsx
+++ app/auth/layout.tsx
@@ -5,12 +5,12 @@ export default function AuthLauout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  return <div className="h-full flex flex-col justify-center items-center">
-    <div className="absolute w-full h-full -z-10">
-    <Image src={"/login.png"} alt="backround image" fill  className="w-full h-full"/>
-    </div>
+  return <div className="h-full flex flex-col justify-center items-center bg-[url('/login.png')] bg-cover bg-center">
+    {/* <div className="absolute w-full h-full -z-10"> */}
+    {/* <Image src={"/login.png"} alt="backround image" fill  className="w-full h-full"/> */}
+    {/* </div> */}
 
-    <div className="flex flex-col  rounded-3xl p-6 w-[400px] bg-white opacity-[97%]">
+    <div className="flex flex-col  rounded-3xl p-6 w-[400px] backdrop-blur-2xl border-white border	">
     <Image src={"/logo.png"} alt="backround image" width={75} height={75} className="m-auto"/>
     
 
